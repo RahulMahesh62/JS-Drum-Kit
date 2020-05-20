f049c781feb4239535eeda74c382dddbc0b9f20a
Rahul Mahesh <41413029+RahulMahesh62@users.noreply.github.com>
2020-05-20 10:31:15 +0530

    Updated CSS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var numberOfDrums = document.querySelectorAll(".drum").length;
 
-for(var i =0; i < numberOfDrums; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+for (var i = 0; i < numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
         var a1 = new Audio("sounds/tom-1.mp3");
         a1.play();
